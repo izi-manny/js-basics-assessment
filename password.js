916fa9c97982b418091cafaadb75eb99ebeaa56f
Emmanuel Izilov <em5750@gmail.com>
2021-09-29 19:47:57 -0400

    Changed to a readline interface

--- password.js
+++ password.js
@@ -1,10 +1,23 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
 console.log('Welcome to the password validator tool!');
 
-const passInput = prompt('Please enter your password');
-console.log(passInput.length)
+// const passInput = global.prompt('Please enter your password');
+// console.log(passInput.length)
 
-if (passInput.length > 10){
-    alert('You have successfully entered a strong password');
-} else {
-    alert('Password did not register. Please enter a password with over 10 characters.')
-}
+reader.question('Please enter your password: \n', function(password) {
+    if (password.length >= 10){
+        console.log('You have successfully entered a strong password');
+    } else {
+        console.log('Password did not register. Please enter a password with over 10 characters.')
+    };
+    reader.close()
+
+});
+
